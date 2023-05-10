a28c89ec84af2322a012679bd37267da4e8d5e72
Alexis Le Baron <alexis.le-baron@epitech.eu>
2023-05-10 13:48:17 +0200

    add choice multi filter

--- django_admin_tailwind/filters.py
+++ django_admin_tailwind/filters.py
@@ -1,10 +1,67 @@
-from django.contrib.admin.utils import get_model_from_relation
-from django.contrib.admin.filters import AllValuesFieldListFilter, RelatedFieldListFilter, FieldListFilter
+from django.contrib.admin.utils import get_model_from_relation, reverse_field_path
+from django.contrib.admin.filters import RelatedFieldListFilter, FieldListFilter
 from django.utils.translation import gettext_lazy as _
 
 
-class DropdownChoiceFilter(AllValuesFieldListFilter):
+class DropdownChoiceFilter(FieldListFilter):
     template = 'django_admin_tailwind/filters/dropdown_choice_filter.html'
+
+    def __init__(self, field, request, params, model, model_admin, field_path):
+        self.lookup_kwarg = field_path
+        self.lookup_kwarg_isnull = '%s__isnull' % field_path
+        lookup_val = params.get(self.lookup_kwarg, [])
+        if lookup_val:
+            lookup_val = lookup_val.split(',')
+        self.lookup_val = lookup_val
+        self.lookup_val_isnull = params.get(self.lookup_kwarg_isnull)
+        self.empty_value_display = model_admin.get_empty_value_display()
+        parent_model, reverse_path = reverse_field_path(model, field_path)
+        # Obey parent ModelAdmin queryset when deciding which options to show
+        if model == parent_model:
+            queryset = model_admin.get_queryset(request)
+        else:
+            queryset = parent_model._default_manager.all()
+        self.lookup_choices = queryset.distinct().order_by(field.name).values_list(field.name, flat=True)
+        super().__init__(field, request, params, model, model_admin, field_path)
+
+    def expected_parameters(self):
+        return [self.lookup_kwarg, self.lookup_kwarg_isnull]
+
+    def choices(self, changelist):
+        yield {
+            'selected': not self.lookup_val and self.lookup_val_isnull is None,
+            'query_string': changelist.get_query_string(remove=[self.lookup_kwarg, self.lookup_kwarg_isnull]),
+            'display': _('All'),
+        }
+        include_none = False
+        for val in self.lookup_choices:
+            if val is None:
+                include_none = True
+                continue
+            val = str(val)
+            if val in self.lookup_val:
+                values = [v for v in self.lookup_val if v != val]
+            else:
+                values = self.lookup_val + [val]
+            if values:
+                yield {
+                    'selected': val in self.lookup_val,
+                    'query_string': changelist.get_query_string({self.lookup_kwarg: ','.join(values)},
+                                                                [self.lookup_kwarg_isnull]),
+                    'display': val,
+                }
+            else:
+                yield {
+                    'selected': val in self.lookup_val,
+                    'query_string': changelist.get_query_string(remove=[self.lookup_kwarg]),
+                    'display': val,
+                }
+        if include_none:
+            yield {
+                'selected': bool(self.lookup_val_isnull),
+                'query_string': changelist.get_query_string({self.lookup_kwarg_isnull: 'True'}, [self.lookup_kwarg]),
+                'display': self.empty_value_display,
+            }
 
 
 class DropdownRelatedFilter(RelatedFieldListFilter):
